07eb83d52b41af216873c97a420c73738d5792df
gerarmedina1708 <gerarmedina1708@gmail.com>
2022-07-01 23:29:00 -0400

    pa ya vamos

--- teoremaofbayes.py
+++ teoremaofbayes.py
@@ -1,8 +1,8 @@
 import requests as rq
 import re
-#import pandas as pd
-#import numpy as np
-#import matplotly as plt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 # REGEX 
@@ -11,22 +11,28 @@ import re
 # {Vulnerabilidad} Probabilidad de que un usuario encuentre una vulnerabilidad del (78%) == 0.78
 # probabilidad de que alguien haga fuzzing, comunmento el {12%} hace fuzzing
 # cual seria la probabilidad de que un usuario encuentre una vulnerabilidad, dado que
-
-
-vuln = 0.78 #probabilidad
-fuzz = 0.12 #fuzz
-entro = 0.010006 #entro
-
-
-
-
-
-resultado = (vuln * (entro/vuln))/entro
-
-print (f'El resultado de esta verga es {resultado}')
 #que ha entrado en nuestra pagina
 # (V)*(White/V) = 
-# white ()
-# p( vulnerabilidad | Entro ) = p(v) . p(e|v) 
-#                                   p(e)
-# 
+#
+# p(hackersPerCountry / country) = p(cvss) x p(hackersPerCountry|cvss) 
+#                                                     p(totalPersonsPerCountryOnInternet)
+
+# tables of avg from country that use internet
+# https://en.wikipedia.org/wiki/List_of_countries_by_number_of_Internet_users 
+
+# Calcules necessaries for the calule the percentage of people per country
+
+usersInternet = 5000000000000 # 100% People on internet.
+totalUsersInRussia = 124000000 # Total users on internet from Russia.
+
+# 100% -------> usersInternet
+#   X  <-------- totalUserInRusssia
+
+## RUSSIA  
+
+cvss = 0.12
+hackersPerCountry = 0.78 
+totalPersonsPerCountryOnInternet = totalUsersInRussia * 100 / usersInternet  #The people in internet on Russia expressed by probability
+
+## /RUSSIA
+print (f'El resultado de esta verga es {totalPersonsPerCountryOnInternet}')
